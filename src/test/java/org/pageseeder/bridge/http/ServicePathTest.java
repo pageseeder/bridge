9e5368c89812c793b4da340e459922700fe3312a
clauret <clauret@weborganic.com>
2016-07-06 17:03:09 +1000

    Updated servive path tests

--- src/test/java/org/pageseeder/bridge/http/ServicePathTest.java
+++ src/test/java/org/pageseeder/bridge/http/ServicePathTest.java
@@ -71,74 +71,74 @@ public class ServicePathTest {
 
   @Test
   public void testToPath_Static() {
-    Assert.assertEquals("/members", new ServicePath("/members").toPath());
-    Assert.assertEquals("/about", new ServicePath("/about").toPath());
+    Assert.assertEquals("/service/members", new ServicePath("/members").toPath());
+    Assert.assertEquals("/service/about", new ServicePath("/about").toPath());
   }
 
   @Test
   public void testToPath_String() {
-    Assert.assertEquals("/members/~abc",   new ServicePath("/members/{member}").toPath("abc"));
-    Assert.assertEquals("/groups/~abc",    new ServicePath("/groups/{group}").toPath("abc"));
-    Assert.assertEquals("/caches/abc",     new ServicePath("/caches/{name}").toPath("abc"));
+    Assert.assertEquals("/service/members/~abc",   new ServicePath("/members/{member}").toPath("abc"));
+    Assert.assertEquals("/service/groups/~abc",    new ServicePath("/groups/{group}").toPath("abc"));
+    Assert.assertEquals("/service/caches/abc",     new ServicePath("/caches/{name}").toPath("abc"));
   }
 
   @Test
   public void testToPath_Numbers() {
     // Integers
-    Assert.assertEquals("/members/123",   new ServicePath("/members/{member}").toPath(123));
-    Assert.assertEquals("/groups/123",    new ServicePath("/groups/{group}").toPath(123));
-    Assert.assertEquals("/uris/123",      new ServicePath("/uris/{uri}").toPath(123));
-    Assert.assertEquals("/caches/123",    new ServicePath("/caches/{name}").toPath(123));
+    Assert.assertEquals("/service/members/123",   new ServicePath("/members/{member}").toPath(123));
+    Assert.assertEquals("/service/groups/123",    new ServicePath("/groups/{group}").toPath(123));
+    Assert.assertEquals("/service/uris/123",      new ServicePath("/uris/{uri}").toPath(123));
+    Assert.assertEquals("/service/caches/123",    new ServicePath("/caches/{name}").toPath(123));
     // Longs
-    Assert.assertEquals("/members/123",   new ServicePath("/members/{member}").toPath(123L));
-    Assert.assertEquals("/groups/123",    new ServicePath("/groups/{group}").toPath(123L));
-    Assert.assertEquals("/uris/123",      new ServicePath("/uris/{uri}").toPath(123L));
-    Assert.assertEquals("/caches/123",    new ServicePath("/caches/{name}").toPath(123L));
+    Assert.assertEquals("/service/members/123",   new ServicePath("/members/{member}").toPath(123L));
+    Assert.assertEquals("/service/groups/123",    new ServicePath("/groups/{group}").toPath(123L));
+    Assert.assertEquals("/service/uris/123",      new ServicePath("/uris/{uri}").toPath(123L));
+    Assert.assertEquals("/service/caches/123",    new ServicePath("/caches/{name}").toPath(123L));
   }
 
   @Test
   public void testToPath_Escape() {
-    Assert.assertEquals("/members/~a%20b", new ServicePath("/members/{member}").toPath("a b"));
-    Assert.assertEquals("/members/~john%40example.org", new ServicePath("/members/{member}").toPath("john@example.org"));
-    Assert.assertEquals("/groups/~a%20b",  new ServicePath("/groups/{group}").toPath("a b"));
-    Assert.assertEquals("/uris/a%20b",    new ServicePath("/uris/{uri}").toPath("a b"));
-    Assert.assertEquals("/caches/a%20b",   new ServicePath("/caches/{name}").toPath("a b"));
+    Assert.assertEquals("/service/members/~a%20b", new ServicePath("/members/{member}").toPath("a b"));
+    Assert.assertEquals("/service/members/~john%40example.org", new ServicePath("/members/{member}").toPath("john@example.org"));
+    Assert.assertEquals("/service/groups/~a%20b",  new ServicePath("/groups/{group}").toPath("a b"));
+    Assert.assertEquals("/service/uris/a%20b",    new ServicePath("/uris/{uri}").toPath("a b"));
+    Assert.assertEquals("/service/caches/a%20b",   new ServicePath("/caches/{name}").toPath("a b"));
   }
 
   @Test
   public void testToPath_EntitiesWithID() {
-    Assert.assertEquals("/members/123",   new ServicePath("/members/{member}").toPath(new PSMember(123L)));
-    Assert.assertEquals("/groups/123",    new ServicePath("/groups/{group}").toPath(new PSGroup(123L)));
+    Assert.assertEquals("/service/members/123",   new ServicePath("/members/{member}").toPath(new PSMember(123L)));
+    Assert.assertEquals("/service/groups/123",    new ServicePath("/groups/{group}").toPath(new PSGroup(123L)));
   }
 
   @Test
   public void testToPath_EntitiesWithName() {
-    Assert.assertEquals("/members/~me",   new ServicePath("/members/{member}").toPath(new PSMember("me")));
-    Assert.assertEquals("/groups/~example-test",    new ServicePath("/groups/{group}").toPath(new PSGroup("example-test")));
+    Assert.assertEquals("/service/members/~me",   new ServicePath("/members/{member}").toPath(new PSMember("me")));
+    Assert.assertEquals("/service/groups/~example-test",    new ServicePath("/groups/{group}").toPath(new PSGroup("example-test")));
   }
 
   @Test(expected = NullPointerException.class)
   public void testToPath_Null() {
-    ServicePath.newServicePath("/members/{member}").toPath(null);
+    ServicePath.newServicePath("/service/members/{member}").toPath(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testToPath_Missing1() {
-    ServicePath.newServicePath("/members/{member}").toPath();
+    ServicePath.newServicePath("/service/members/{member}").toPath();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testToPath_Missing2() {
-    ServicePath.newServicePath("/members/{member}/comments/{comment}").toPath();
+    ServicePath.newServicePath("/service/members/{member}/comments/{comment}").toPath();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testToPath_Extra1() {
-    ServicePath.newServicePath("/members").toPath("me");
+    ServicePath.newServicePath("/service/members").toPath("me");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testToPath_Extra2() {
-    ServicePath.newServicePath("/members/{member}").toPath("me", "you");
+    ServicePath.newServicePath("/service/members/{member}").toPath("me", "you");
   }
 }
