ff87ca25e34b177186eb70d0acfef7ae74ed30b3
clauret <clauret@weborganic.com>
2026-01-30 10:46:58 +1100

    Chore: Configure Java toolchain to ensure compatibility with Java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ allprojects {
     withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+      languageVersion.set(JavaLanguageVersion.of(8))
+    }
   }
 
   repositories {
