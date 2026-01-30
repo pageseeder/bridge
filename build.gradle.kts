fde9aa9f792f6979929a51cf278679cef4e098ea
clauret <clauret@weborganic.com>
2026-01-30 17:46:14 +1100

    Chore: Update Java version compatibility to 11 in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ allprojects {
   configure<JavaPluginExtension> {
     withJavadocJar()
     withSourcesJar()
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
     toolchain {
-      languageVersion.set(JavaLanguageVersion.of(8))
+      languageVersion.set(JavaLanguageVersion.of(11))
     }
   }
 
