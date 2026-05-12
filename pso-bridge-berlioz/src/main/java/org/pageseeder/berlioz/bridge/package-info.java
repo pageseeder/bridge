b849bff5a72de2b7cc155719ceb6aba86c78978e
clauret <clauret@weborganic.com>
2026-05-12 21:13:07 +1000

    Chore: Remove deprecated `org.pageseeder.berlioz.bridge` package and associated classes

--- pso-bridge-berlioz/src/main/java/org/pageseeder/berlioz/bridge/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 1999-2014 allette systems pty. ltd.
- */
-/**
- * Top level package for the Deck library
- *
- * @author christophe Lauret
- */
-package org.pageseeder.berlioz.bridge;
